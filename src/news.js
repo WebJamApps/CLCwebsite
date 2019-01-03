d4081b5d8f06015a563aed77888effbba486697f
“Joshua <joshua.v.sherman@gmail.com>
2019-01-03 08:04:38 -0500

    removes urlsearchparams

--- src/news.js
+++ src/news.js
@@ -33,13 +33,13 @@ export class Beliefs {
     }
     this.books = booksArr;
   }
-  attached() {
-    this.searchParams = new URLSearchParams(window.location.search);
-    if (this.searchParams.get('reload')) {
-      /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'test') window.location.href = window.location.href.split('?')[0];
-    }
-    this.top = document.getElementsByClassName('material-header')[0];
-    if (this.top !== null && this.top !== undefined) this.top.scrollIntoView();
-  }
+  // attached() {
+  //   this.searchParams = new URLSearchParams(window.location.search);
+  //   if (this.searchParams.get('reload')) {
+  //     /* istanbul ignore if */
+  //     if (process.env.NODE_ENV !== 'test') window.location.href = window.location.href.split('?')[0];
+  //   }
+  //   this.top = document.getElementsByClassName('material-header')[0];
+  //   if (this.top !== null && this.top !== undefined) this.top.scrollIntoView();
+  // }
 }
--- test/karma-unit/home.spec.js
+++ test/karma-unit/home.spec.js
@@ -11,20 +11,20 @@ describe('the Home Module', () => {
     done();
   });
 
-  it('scrolls to top of page on page load', (done) => {
-    document.body.innerHTML = '<div class="material-header content-block"></div>';
-    home.attached();
-    expect(home.top).not.toBe(null);
-    document.body.innerHTML = '<div class="content-block"></div>';
-    home.top = null;
-    home.attached();
-    expect(home.top).toBe(undefined);
-    done();
-  });
-  it('attaches to the dom with a reload in the url', (done) => {
-    document.body.innerHTML = '<div class="home content-block"><div class="topSection"></div><input class="loginemail"/></div>';
-    window.history.pushState({}, 'home', '/?reload=true');
-    home.attached();
-    done();
-  });
+  // it('scrolls to top of page on page load', (done) => {
+  //   document.body.innerHTML = '<div class="material-header content-block"></div>';
+  //   home.attached();
+  //   expect(home.top).not.toBe(null);
+  //   document.body.innerHTML = '<div class="content-block"></div>';
+  //   home.top = null;
+  //   home.attached();
+  //   expect(home.top).toBe(undefined);
+  //   done();
+  // });
+  // it('attaches to the dom with a reload in the url', (done) => {
+  //   document.body.innerHTML = '<div class="home content-block"><div class="topSection"></div><input class="loginemail"/></div>';
+  //   window.history.pushState({}, 'home', '/?reload=true');
+  //   home.attached();
+  //   done();
+  // });
 });
