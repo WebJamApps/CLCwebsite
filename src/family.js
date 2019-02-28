a6bd43fb5c4489c6c8f6db935223b68a11bd166c
Joshua Sherman <jsherman@valcom.com>
2019-02-28 16:30:52 -0500

    fixes family page

--- src/family.js
+++ src/family.js
@@ -12,37 +12,36 @@ export class Family {
     this.app = app;
     this.top = null;
     this.familyContent = { title: '', comments: '' };
+    this.slideshowImages = [];
   }
-  slideshowImages = [
-    // { src: 'https://dl.dropboxusercontent.com/s/xeq3ty7tzltrl1y/21_.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/2r4x7ipuczoon0s/image022.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/4kk6isxtjwec2bn/image023.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/916h50ubbd5u8rt/image024.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/k6rdkrl7ma63417/image025.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/a8crhcv2cbn3oao/image026.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/e9hybdzjmn8tchg/image027.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/qowexq8um5v1ve6/image028.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/5hn0wzcz2b06dwr/image029.jpg?dl=0', style: 'max-height:5in' },
-    // { src: 'https://dl.dropboxusercontent.com/s/k5cd13bl2w8zb3f/image030.jpg?dl=0', style: 'max-height:5in' }
-  ];
 
   get widescreenHomepage() { return document.documentElement.clientWidth > 1200; }
   async activate() {
     let res, picUrls;
     try {
-      res = await this.app.httpClient.fetch('/book/getFamilyContent');
-    } catch (e) { console.log(e.message); }
-    if (res !== null && res !== undefined) this.familyContent = await res.json();
-    try {
-      picUrls = await this.app.httpClient.fetch('/book/getFamilyPics');
-    } catch (e) { console.log(e.message); }
-    console.log('fetched');
-    if (picUrls !== null && picUrls !== undefined) {
-      picUrls = await picUrls.json();
-      console.log(picUrls);
-      this.slideshowImage = [];
-      for (let i = 0; i < picUrls.length; i += 1) this.slideshowImages.push({ src: picUrls[i].comments });
-      console.log(this.slideshowImages);
+      res = await this.app.httpClient.fetch('/book/findOne?type=familyPageContent');
+      if (res !== null && res !== undefined) this.familyContent = await res.json();
+      picUrls = await this.app.httpClient.fetch('/book?type=familyPics');
+      if (picUrls !== null && picUrls !== undefined) picUrls = await picUrls.json();
+    } catch (e) { sessionStorage.setItem('familyError', `${e.message}`); }
+    // try {
+    //
+    // } catch (e) { console.log(e.message); }
+    // console.log('fetched');
+    // if (picUrls !== null && picUrls !== undefined) {
+    //   picUrls = await picUrls.json();
+    //   console.log(picUrls);
+    //   this.slideshowImage = [];
+    //   for (let i = 0; i < picUrls.length; i += 1) this.slideshowImages.push({ src: picUrls[i].comments });
+    //   console.log(this.slideshowImages);
+    // }
+    return this.setFamilyPics(picUrls);
+  }
+  setFamilyPics(picUrls) {
+    this.slideshowImages = [];
+    for (let i = 0; i < picUrls.length; i += 1) {
+      if (picUrls[i].url === null || picUrls[i].url === undefined || picUrls[i].url === '') picUrls[i].url = picUrls[i].comments;
+      this.slideshowImages.push({ src: picUrls[i].url });
     }
   }
 }
