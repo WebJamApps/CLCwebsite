acbef4dc5e7028f4fd68383ec8e860196d8a43d3
Rhayven <amayis@ymail.com>
2019-08-02 03:36:50 -0400

    Linting, Tests not running properly should be fixed

--- src/components/pic-slider.jsx
+++ src/components/pic-slider.jsx
@@ -29,7 +29,7 @@ export class PicSlider {
       <Slider {...this.settings}>
         {this.data.map((data, key) => (
           <div key={key} style={data.style}>
-            <img alt="img" padding-top="56.25%" width="100%" src={data.src} />
+            <img className="slide-show" alt="img" src={data.src} />
           </div>
         ))}
       </Slider>
